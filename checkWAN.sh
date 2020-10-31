ab4c3c2e2ee13abf78efc307e81ffba6005404cd
Matthew Gomez <magomez96@users.noreply.github.com>
2020-10-30 21:57:07 -0400

    Update checkWAN.sh

--- checkWAN.sh
+++ checkWAN.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
-# in my setup, igb2 is the Tier-2 gateway. The Tier-1 gateway is igb0.
-# This script would be most likely be used with a data-capped Tier-2
-# WAN connection. This script will check for states on the Tier-2 gateway
-# while the Tier-1 gateway is active, and if any exist it will remove them. 
-
+# In my setup, igb2 is the Tier-2 gateway. The Tier-1 gateway is igb0
 WAN_CHECK_INTERVAL=${1:-60}
 PRI_WAN="igb0"
 BACK_WAN="igb2"
+BACK_WAN_IP="$(/sbin/ifconfig $BACK_WAN | /usr/bin/awk '/inet /{print $2}')"
 
 while true; do
 sleep "$WAN_CHECK_INTERVAL"
-	CURRENT_GW="$(netstat -rn4 | awk '/default/{print $4}')"
+	CURRENT_GW="$(/usr/bin/netstat -rn4 | /usr/bin/awk '/default/{print $4}')"
 	if [ "$CURRENT_GW" = $PRI_WAN ]; then
-		WAN2_STATES="$(pfctl -i $BACK_WAN -s states | grep -v NO_TRAFFIC)"
+        WAN2_STATES="$(/sbin/pfctl -i $BACK_WAN -s states | /usr/bin/grep -v NO_TRAFFIC)"
 		if [ -n "$WAN2_STATES" ]; then
-			/etc/rc.kill_states $BACK_WAN
-		fi
+			/sbin/pfctl -k "$BACK_WAN_IP" > /dev/null
+                fi
 	fi
 done
